5ca50f1d63f9244cbe1d09429220132254447a53
SAMahlangu <siphoalex955@gmail.com>
2024-08-27 14:48:14 +0200

    Add React project files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Projects from './pages/Projects';
+import './styles.css';  // Import global styles
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <header>
+                    <h1>AI in AGRICULTURE</h1>
+                </header>
+                <nav>
+                    <ul>
+                        
+                        <li><Link to="/">Projects</Link></li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Projects />} />
+                 
+                </Routes>
+                
+            </div>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/pages/Projects.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './Projects.css';
+import project1 from '../assets/images/damaged_leaves_1920x870.jpg';
+import project2 from '../assets/images/plants.jpg';
+import project3 from '../assets/images/Plant-nutrients.jpg';
+import project4 from '../assets/images/weed.jpg';
+import project5 from '../assets/images/pigsCount.jpg';
+import project6 from '../assets/images/soilAnalysis.jpg';
+import project7 from '../assets/images/irrigation.jpg';
+import project8 from '../assets/images/aquaculture.jpg';
+
+
+
+
+
+const Projects = () => {
+    return (
+        <div className="projects-container">
+
+            <div className="projects-grid">
+                <div className="project-item">
+                    <a href="http://16.171.165.242/" target="_blank" rel="noopener noreferrer">
+                        <img src={project1} alt="Project 1" />
+                    </a>
+                    <h2>Plant Disease Detection</h2>
+                </div>
+                <div className="project-item">
+                    <a href="http://13.53.174.112/" target="_blank" rel="noopener noreferrer">
+                        <img src={project2} alt="Project 2" />
+                    </a>
+                    <h2>Plant Species Detection</h2>
+                </div>
+                <div className="project-item">
+                    <a href="http://13.60.88.8/" target="_blank" rel="noopener noreferrer">
+                        <img src={project3} alt="Project 3" />
+                    </a>
+                    <h2>Plant Nutrient Difficiency</h2>
+                </div>
+                <div className="project-item">
+                    <a href="http://13.53.187.119/" target="_blank" rel="noopener noreferrer">
+                        <img src={project4} alt="Project 4" />
+                    </a>
+                    <h2>Weeds Detection</h2>
+                </div>
+            </div>
+		<div className="projects-grid">
+	                <div className="project-item">
+	                    <a href="https://huggingface.co/spaces/mdalakeys/diabeticsRec" target="_blank" rel="noopener noreferrer">
+                        	<img src={project5} alt="Project 5" />
+                	    </a>
+        	            <h2>Animal Count</h2>
+	                </div>
+			<div className="project-item">
+	                    <a href="http://13.61.35.167/" target="_blank" rel="noopener noreferrer">
+                        	<img src={project6} alt="Project 6" />
+                	    </a>
+        	            <h2>Soil Analysis</h2>
+	                </div>
+			<div className="project-item">
+	                    <a href="https://huggingface.co/spaces/mdalakeys/diabeticsRec" target="_blank" rel="noopener noreferrer">
+                        	<img src={project7} alt="Project 5" />
+                	    </a>
+        	            <h2>Irrigation Analysis</h2>
+	                </div>
+			<div className="project-item">
+	                    <a href="http://fish-farming-chatbot.onrender.com/" target="_blank" rel="noopener noreferrer">
+                        	<img src={project8} alt="Project 5" />
+                	    </a>
+        	            <h2>Aquaculture</h2>
+	                </div>	
+	        </div>
+        </div>
+    );
+};
+
+export default Projects;
